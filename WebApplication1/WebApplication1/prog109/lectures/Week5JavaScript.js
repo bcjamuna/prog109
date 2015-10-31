ff028a1e7a7ae803310bb2f8d5b3df2fd81782c4
bcjamuna <jamuna.blumette@bellevuecollege.edu>
2015-10-30 23:03:39 -0700

    stylized first javascript and changed image on liquid layout

---
+++ WebApplication1/WebApplication1/prog109/lectures/Week5JavaScript.js
@@ -0,0 +1,17 @@
+﻿//this is my first javascript
+/* 
+Created by: J. Blumette
+Date: 10/30/15
+*/
+
+alert("Welcome User!");
+
+var myResults;
+myResults =
+  document.getElementById('result');
+
+var firstname =
+    prompt("What's your first name?", "Enter only letters");
+
+myResults.textContent =
+  "Welcome " + firstname + "!";
